1e29c484b634c0fbb9fee1071a3657782bb325d2
Mitchell Rosen <mitchellwrosen@gmail.com>
2023-03-16 13:37:06 -0400

    fix compilation errors in test suite

--- src/stm-fsifo/test/Main.hs
+++ src/stm-fsifo/test/Main.hs
@@ -70,7 +70,7 @@ prop_queue = property do
               <$> liftIO
                 ( atomically do
                     q <- readTVar qvar
-                    Fsifo.push q n
+                    Fsifo.pushFsifo q n
                 )
           upd = Update \(QueueModel s pushCount im _) (Push i) out ->
             let !pushCount' = pushCount + 1
@@ -88,7 +88,7 @@ prop_queue = property do
           execute Pop = do
             liftIO $ atomically do
               q <- readTVar qvar
-              Fsifo.pop q
+              Fsifo.popFsifo q
           upd = Update \(QueueModel s pushCount im _) Pop _out ->
             let mk = fst <$> IM.lookupGE minBound s
                 s' = maybe s (\k -> IM.delete k s) mk
